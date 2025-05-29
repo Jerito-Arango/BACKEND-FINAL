3e8028d7b96ae82b61788e717b1e56acfe5b24e1
Jerónimo Arango <jerito@Jeronimos-MacBook-Air.local>
2025-05-28 20:50:44 -0500

    ya casi

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // 🔧 CORS configurado explícitamente
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://tufrontend.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
